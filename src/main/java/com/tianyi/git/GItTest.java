2cf3659dac7002e1af474cf7e6893cb44da7882b
tianyi <tianyi@henian.com>
2024-07-06 17:37:55 +0800

    second commit

--- src/main/java/com/tianyi/git/GItTest.java
+++ src/main/java/com/tianyi/git/GItTest.java
@@ -3,5 +3,6 @@ package com.tianyi.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
